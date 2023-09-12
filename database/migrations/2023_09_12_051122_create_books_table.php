feede6e8d045a0cd9f3d1b361de31154fbebf51b
jukemori <jukemori@gmail.com>
2023-09-12 16:50:56 +0900

    set controller

--- database/migrations/2023_09_12_051122_create_books_table.php
+++ database/migrations/2023_09_12_051122_create_books_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('author');
+            $table->date('publish_date');
             $table->timestamps();
         });
     }
